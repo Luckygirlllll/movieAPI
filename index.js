e184260c35c98ecb2595eee20995532ad9680a23
Iryna Kanivets <irynakanivets@gmail.com>
2020-04-07 21:48:13 +0300

    Added user routes

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 const express = require('express');
+
 const app = express();
 
 mongoose.connect('mongodb://localhost/vidly')
@@ -15,6 +17,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ routes/users.js
@@ -0,0 +1,32 @@
+const _ = require('lodash')
+const bcrypt = require("bcrypt");
+const {User, validate} = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+
+    
+    let user =await User.findOne({ email: req.body.email})
+    console.log("user ", user);
+    if(user) return res.status(400).send("User already registered.")
+
+    user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    })
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt)
+  
+    await user.save();
+
+    user =_.pick(user, ['_id','name', 'email'])
+    
+    res.send(user);
+});
+
+module.exports = router;
