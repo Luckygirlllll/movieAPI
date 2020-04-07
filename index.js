85b6098d627ee6cd888b6b6379a0c6964b63106b
Iryna Kanivets <irynakanivets@gmail.com>
2020-04-07 22:05:08 +0300

    Added auth route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const express = require('express');
 
 const app = express();
@@ -18,6 +19,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
