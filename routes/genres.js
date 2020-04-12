9c12d9f94265e79bdbe04b89d946da18f574eb12
Iryna Kanivets <irynakanivets@gmail.com>
2020-04-12 22:19:52 +0300

    added admin role check

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,5 @@
 const auth = require('../middleware/auth')
+const admin = require('../middleware/admin')
 const {Genre, validate} = require('../models/genre');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -33,7 +34,7 @@ router.put('/:id', async (req, res) => {
   res.send(genre);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const auth = require('../middleware/auth')
 const config = require("config")
 const jwt = require("jsonwebtoken")
 const _ = require('lodash')
@@ -7,11 +8,15 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
+router.get('/:me', auth, async(req, res) => {
+   const user = await User.findById(req.user._id).select('-password');
+   res.send(user); 
+})
+
 router.post('/', async (req, res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
 
-    
     let user =await User.findOne({ email: req.body.email})
     console.log("user ", user);
     if(user) return res.status(400).send("User already registered.")
