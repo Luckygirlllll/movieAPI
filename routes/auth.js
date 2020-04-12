8fb4a77ab21bdd21d861e6662c3b4e715660c21c
Iryna Kanivets <irynakanivets@gmail.com>
2020-04-12 20:03:00 +0300

    added auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
+const Joi = require("joi")
 const _ = require('lodash')
 const bcrypt = require("bcrypt");
-const {User, validate} = require('../models/user');
+const {User} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
@@ -9,16 +10,13 @@ router.post('/', async (req, res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
 
-    
-    let user =await User.findOne({ email: req.body.email})
-    console.log("user ", user);
+    let user = await User.findOne({ email: req.body.email})
     if(!user) return res.status(400).send("Invalid email or password.")
 
-    const validPassword = bcrypt.compare(req.body.password, user.password)
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
     if(!validPassword) return res.status(400).send("Invalid email or password.")
 
     res.send(true)
-   
 });
 
 
@@ -26,7 +24,6 @@ function validate(req) {
     const schema = {
       email: Joi.string().min(5).max(255).required().email(),
       password: Joi.string().min(5).max(1024).required(),
-  
     };
   
     return Joi.validate(req, schema);
